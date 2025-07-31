2a6158b68fe85b80faccbd156a851bb434b4fb16
Amresh-01 <amreshchaurasiya373@gmail.com>
2025-07-31 09:49:39 +0530

    Add first_letter_to_Uppercase and String_compression Solutions

--- Stacks and Queues/Stack.java
+++ Stacks and Queues/Stack.java
@@ -1,45 +1,37 @@
 
-
-
-
-                                        //       Anagram String
-
-
+//       Anagram String
 import java.util.*;
 
-public class Stack{
+public class Stack {
 
-        public static void main(String args[]){
-                Scanner sc = new Scanner(System.in);
-                String str1 = sc.nextLine(); 
-                String str2 = sc.nextLine();
-                
-                if(str1.length() != str2.length()){
-                        System.out.print(false);
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        if (str1.length() != str2.length()) {
+            System.out.print(false);
+        } else {
+
+            char str1char[] = str1.toCharArray();     // String ko Array mai convert kar diya.
+            char str2char[] = str2.toCharArray();
+            Arrays.sort(str1char);       // Array ko sort(Ascending or descending order) kar diya hai.
+            Arrays.sort(str2char);
+
+            int flag = 0;
+
+            for (int i = 0; i < str1.length(); i++) {
+                if (str1char[i] != str2char[i]) {
+                    flag = 1;
+                    break;
                 }
-
-                else{
-
-                       char str1char[] = str1.toCharArray();     // String ko Array mai convert kar diya.
-                       char str2char[] = str2.toCharArray();
-                       Arrays.sort(str1char);       // Array ko sort(Ascending or descending order) kar diya hai.
-                       Arrays.sort(str2char);
-
-                       int flag = 0;
-
-                for (int i = 0; i < str1.length(); i++) {
-                    if (str1char[i] != str2char[i]) {
-                        flag = 1;
-                        break;
-                    }
-                }
-                if (flag==0) {
-                        System.out.println(true);
-                }
-                else{ 
-                        System.out.println(false);
-                }
+            }
+            if (flag == 0) {
+                System.out.println(true);
+            } else {
+                System.out.println(false);
+            }
 
         }
-}
+    }
 }
